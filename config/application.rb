7647a3ada969a3f96fa32acf6f836fa13cbd838a
Manfredi <manfbenvenuti@gmail.com>
2017-02-10 21:31:18 +0100

    Fixed email objects

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module EntradedDemo
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    #Enable Rails 4 to serve your assets on Heroku
+    config.serve_static_assets = true
   end
 end
